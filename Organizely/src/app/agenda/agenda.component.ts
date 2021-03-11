b7fbddf8317763e8673537b77bf1c05c93da3da9
Angela Mitchell <anglarenmit@gmail.com>
2021-03-10 18:40:41 -0600

    Add FullCalendar plugin to start building agenda view

--- Organizely/src/app/agenda/agenda.component.ts
+++ Organizely/src/app/agenda/agenda.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { COURSES } from '../shared/mock-data/mock-courses';
 import { Course } from '../shared/models/course.model';
+import { CalendarOptions } from '@fullcalendar/angular';
+import listPlugin from '@fullcalendar/list';
 
 @Component({
   selector: 'app-agenda',
@@ -8,6 +10,18 @@ import { Course } from '../shared/models/course.model';
   styleUrls: ['./agenda.component.css'],
 })
 export class AgendaComponent implements OnInit {
+  calendarVisible: boolean = true;
+  calendarOptions: CalendarOptions = {
+    plugins: [listPlugin],
+    initialView: 'listWeek',
+    events: [
+      {
+        title: 'LC101',
+        start: '2021-03-10T17:30:00',
+      },
+    ],
+  };
+
   dates: Date[] = [
     new Date(2021, 2, 7),
     new Date(2021, 2, 8),
@@ -25,8 +39,8 @@ export class AgendaComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    this.sortCoursesByDay();
-    console.log(this.coursesByDay);
+    // this.sortCoursesByDay();
+    // console.log(this.coursesByDay);
   }
 
   sortCoursesByDay() {
--- Organizely/src/app/app.module.ts
+++ Organizely/src/app/app.module.ts
@@ -1,20 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import listPlugin from '@fullcalendar/list';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgendaComponent } from './agenda/agenda.component';
 
+FullCalendarModule.registerPlugins([listPlugin]);
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    AgendaComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, AgendaComponent],
+  imports: [BrowserModule, AppRoutingModule, FullCalendarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
