76c87996cc6b2c3553339a81e424d122c5871c47
Angela Mitchell <anglarenmit@gmail.com>
2021-03-17 16:59:03 -0500

    Make add course form reactive

--- Organizely/src/app/classes-page/classes-page.component.ts
+++ Organizely/src/app/classes-page/classes-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-classes-page',
@@ -6,14 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./classes-page.component.css'],
 })
 export class ClassesPageComponent implements OnInit {
-  dayNames: string[] = [
-    'Sunday',
-    'Monday',
-    'Tuesday',
-    'Wednesday',
-    'Thursday',
-    'Friday',
-    'Saturday',
+  dayNames: object[] = [
+    { name: 'Sunday', id: 0 },
+    { name: 'Monday', id: 1 },
+    { name: 'Tuesday', id: 2 },
+    { name: 'Wednesday', id: 3 },
+    { name: 'Thursday', id: 4 },
+    { name: 'Friday', id: 5 },
+    { name: 'Saturday', id: 6 },
   ];
 
   currentDate: Date = new Date();
@@ -21,7 +22,33 @@ export class ClassesPageComponent implements OnInit {
 
   semesterSeasons: string[] = ['Fall', 'Winter', 'Spring', 'Summer'];
 
+  addCourseForm: FormGroup;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.initForm();
+  }
+
+  initForm() {
+    this.addCourseForm = new FormGroup({
+      courseName: new FormControl(null, Validators.required),
+      startTime: new FormControl(null, Validators.required),
+      endTime: new FormControl(null, Validators.required),
+      startRecur: new FormControl(null, Validators.required),
+      endRecur: new FormControl(null, Validators.required),
+      daysOfWeek: new FormArray([]),
+      semesterSeason: new FormControl(null, Validators.required),
+      semesterYear: new FormControl(null, Validators.required),
+      teacherName: new FormControl(null),
+    });
+  }
+
+  onCheckboxChange(event) {
+    console.log(event.target.value);
+  }
+
+  submit() {
+    console.log(this.addCourseForm.value);
+  }
 }
