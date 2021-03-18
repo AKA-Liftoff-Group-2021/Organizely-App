c0730a8100fcb4a2ff8f956481ec836618595694
Angela Mitchell <anglarenmit@gmail.com>
2021-03-18 13:59:52 -0500

    Remove id properties; change to class structure

--- Organizely/src/app/shared/models/student.model.ts
+++ Organizely/src/app/shared/models/student.model.ts
@@ -1,6 +1,11 @@
 export class Student {
-  studentId: number;
-  username: string;
-  firstName: string;
-  lastName: string;
+  public username: string;
+  public firstName: string;
+  public lastName: string;
+
+  constructor(username: string, firstName: string, lastName: string) {
+    this.username = username;
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
 }
