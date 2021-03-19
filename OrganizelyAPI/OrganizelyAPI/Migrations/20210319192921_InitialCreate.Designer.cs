064d8a1d16af0426e4fc9a13107541fa2eee20bf
Angela Mitchell <anglarenmit@gmail.com>
2021-03-19 16:08:26 -0500

    Create add class button for agenda view

--- OrganizelyAPI/OrganizelyAPI/Migrations/20210319192921_InitialCreate.Designer.cs
+++ OrganizelyAPI/OrganizelyAPI/Migrations/20210319192921_InitialCreate.Designer.cs
@@ -9,7 +9,7 @@ using OrganizelyAPI.Data;
 namespace OrganizelyAPI.Migrations
 {
     [DbContext(typeof(StudentDbContext))]
-    [Migration("20210316200058_InitialCreate")]
+    [Migration("20210319192921_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
