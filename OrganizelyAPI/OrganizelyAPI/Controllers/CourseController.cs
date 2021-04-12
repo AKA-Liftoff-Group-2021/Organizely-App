3a4003fe5510bac01e0613f8831795db1e9b1ee6
Maricar Walters <walterskai7@gmail.com>
2021-04-12 13:42:07 -0500

    Successfully added studentdb file in cspro folder(api folder) under data folder

--- OrganizelyAPI/OrganizelyAPI/Controllers/CourseController.cs
+++ OrganizelyAPI/OrganizelyAPI/Controllers/CourseController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,7 @@ using OrganizelyAPI.ViewModels;
 
 namespace OrganizelyAPI.Controllers
 {
-    [Authorize]//(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class CourseController : ControllerBase
