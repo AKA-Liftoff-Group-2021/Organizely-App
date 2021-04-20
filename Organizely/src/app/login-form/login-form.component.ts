3c112b2e7829536cc8115756787178eea8776581
Angela Mitchell <anglarenmit@gmail.com>
2021-04-19 23:05:35 -0500

    Start of user login auth

--- Organizely/src/app/login-form/login-form.component.ts
+++ Organizely/src/app/login-form/login-form.component.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
+    if (localStorage.getItem('token') != null) {
+      this.router.navigate(['/', 'organizely', 'home']);
+    }
   }
 
   onLogin(loginForm: NgForm) {
-    console.log(loginForm.value);
+    this.authService.login(loginForm.value).subscribe(
+      (res: any) => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/', 'organizely', 'home']);
+      },
+      (err) => {
+        if (err.status == 400) {
+          console.log('Incorrect username or password');
+        } else {
+          console.log(err);
+        }
+      }
+    );
   }
-
 }
