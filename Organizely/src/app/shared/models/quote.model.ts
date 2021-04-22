5b0cae39a5f3dc8a4c237a1827002643ce8a7588
Angela Mitchell <anglarenmit@gmail.com>
2021-04-22 10:35:14 -0500

    Rename studentId to userId; add userId in comments to models; add email to user model

--- Organizely/src/app/shared/models/quote.model.ts
+++ Organizely/src/app/shared/models/quote.model.ts
@@ -1,15 +1,18 @@
 export class Quote {
-    public quoteId: number;
-    public content: string;
-    public author: string;
+  public quoteId: number;
+  public content: string;
+  public author: string;
+  // public userId: string;
 
-    constructor(
-        quoteId: number,
-        content: string,
-        author: string
-      ) {
-        this.quoteId = quoteId;
-        this.content = content;
-        this.author = author;
-      }
+  constructor(
+    quoteId: number,
+    content: string,
+    author: string
+    // userId: string
+  ) {
+    this.quoteId = quoteId;
+    this.content = content;
+    this.author = author;
+    // this.userId = userId;
+  }
 }
--- Organizely/src/app/shared/models/user.model.ts
+++ Organizely/src/app/shared/models/user.model.ts
@@ -1,11 +1,18 @@
-export class Student {
+export class User {
   public username: string;
   public firstName: string;
   public lastName: string;
+  public email: string;
 
-  constructor(username: string, firstName: string, lastName: string) {
+  constructor(
+    username: string,
+    firstName: string,
+    lastName: string,
+    email: string
+  ) {
     this.username = username;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.email = email;
   }
 }
