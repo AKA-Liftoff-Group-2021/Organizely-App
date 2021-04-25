9ce5818689159866ef2bbe10eb94876a2b015e9f
Angela Mitchell <anglarenmit@gmail.com>
2021-04-25 14:34:58 -0500

    Add userId to more models; revised mock data; add userId to request bodies

--- Organizely/src/app/shared/mock-data/mock-tasks.ts
+++ Organizely/src/app/shared/mock-data/mock-tasks.ts
@@ -5,12 +5,14 @@ export const STUDENTTASKS: StudentTask[] = [
     studentTaskId: 1,
     studentTaskName: 'Do homework',
     priority: 'High',
+    userId: '1',
     taskDueDate: new Date(2021, 3, 5),
   },
   {
     studentTaskId: 2,
     studentTaskName: 'Practice coding',
     priority: 'High',
+    userId: '1',
     taskDueDate: new Date(2021, 2, 29),
   },
 ];
--- Organizely/src/app/shared/models/student-task.model.ts
+++ Organizely/src/app/shared/models/student-task.model.ts
@@ -2,20 +2,20 @@ export class StudentTask {
   public studentTaskId: number;
   public studentTaskName: string;
   public priority: string;
-  // public userId: string;
+  public userId: string;
   public taskDueDate?: Date;
 
   constructor(
     studentTaskId: number,
     studentTaskName: string,
     priority: string,
-    // userId: string,
+    userId: string,
     taskDueDate?: Date
   ) {
     this.studentTaskId = studentTaskId;
     this.studentTaskName = studentTaskName;
     this.priority = priority;
-    // this.userId = userId;
+    this.userId = userId;
     this.taskDueDate = taskDueDate;
   }
 }
